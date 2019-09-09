a9c661c0e6c57b93ec8cfc4e4c4075b58ca5f2a1
Praveen27Patil <pm.patil4u@gmail.com>
2019-09-09 18:06:25 +0530

    Auto build after commit

--- DemoTest/src/HelloPrint.java
+++ DemoTest/src/HelloPrint.java
@@ -5,7 +5,7 @@ public class HelloPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=0; i<10; i++) {
-			System.out.println("Print i: "+i);
+			System.out.println("Print i::::::: "+i);
 		}
 	}
 
