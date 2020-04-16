b8044b0d6c29060a4cbbd10fc474c584c956153a
Praveen27Patil <pm.patil4u@gmail.com>
2020-04-16 19:09:50 +0530

    added 2 line

--- DemoTest/src/HelloPrint.java
+++ DemoTest/src/HelloPrint.java
@@ -7,6 +7,7 @@ public class HelloPrint {
 		for(int i=0; i<7; i++) {
 			System.out.println("Print i: "+i);
 			System.out.println("Added new line")
+			System.out.println("Added 2 new line")
 		}
 	}
 
